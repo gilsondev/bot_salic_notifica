2fb085e22911129df0bc3ab95df97d906a690727
guiamado <amado.guigui@gmail.com>
2017-10-20 11:46:21 -0200

    [UPDATE] adicionando comentarios

--- bot.py
+++ bot.py
@@ -16,21 +16,23 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 checkExist = '[(0,), (0,), (0,), (0,), (0,), (0,), (0,), (0,), (0,), (0,), (0,), (0,), (0,), (0,), (0,)]'
-
+# Função que contém o algoritmo para pegar o buffer de PRONACs e fazer conferência se ja existem
 def alarm(bot, job):
+    # fazendo conexão com banco de dados
     conn=sqlite3.connect('bot.db')
-    
+    # Coletando da API do salic as informações dos ultimos PRONCAs
     with urllib.request.urlopen("http://api.salic.cultura.gov.br/v1/projetos/?limit=15&sort=PRONAC:desc&format=json") as url:
         data = json.loads(url.read().decode('utf-8'))
     
     array = ([])
     noticia = data 
-    
+    # Colocando os dados recebidos da API em um array 
     for i in range(15):
         array.append(noticia['_embedded']['projetos'][i]['PRONAC'])
     
 
     y = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14)
+    # Laço feito para conferência se há novidades e então mandar a mensagem do bot
     for x in reversed(y):
         
         menssagem =  'Nova Proposta de #Projeto aceita pelo MinC:' + '\n\n' + 'Nome do Projeto: ' + noticia['_embedded']['projetos'][x]['nome'] +'\n\n'+ 'Pronac do Projeto: ' + noticia['_embedded']['projetos'][x]['PRONAC'] +'\n\n'+ 'Area do Projeto: ' + noticia['_embedded']['projetos'][x]['area'] +'\n\n'+ 'Segmento: ' + noticia['_embedded']['projetos'][x]['segmento'] + '\n\n'+ 'Cidade: '+ noticia['_embedded']['projetos'][x]['municipio']+'-'+ noticia['_embedded']['projetos'][x]['UF'] +'\n\n'+ 'Valor da Proposta: R$ '+ str(noticia['_embedded']['projetos'][x]['valor_proposta'])+'\n\n'+ 'Resumo do Projeto: ' + noticia['_embedded']['projetos'][x]['resumo'] +'\n\n'+ 'Acompanhe a execução deste projeto no Versalic em:\n' + 'http://versalic.cultura.gov.br/#/projetos/'+ noticia['_embedded']['projetos'][x]['PRONAC'] +'\n\n'+ 'Mais sobre a Lei Rouanet em Rouanet.cultura.gov.br'
@@ -40,11 +42,12 @@ def alarm(bot, job):
         curs = conn.cursor()
         teste = curs.execute(sql,params)
         teste2 = curs.fetchall()
-        teste3 = curs.fetchone()
-        
+
+        # Verificando se existe no Banco
         if checkExist == str(teste2):
             
             bot.sendMessage(job.context, text=menssagem)
+    # Laço feito para guardar no banco a ultima atualização da API para criação de um buffer
     for p in range(15):
 
         params1 =  (noticia['_embedded']['projetos'][p]['PRONAC'],(p+1))
@@ -57,7 +60,7 @@ def alarm(bot, job):
     conn.close()
 
 
-
+# Função onde é feita a conta do tempo de quando a função que manda mensagem será chamada
 def set(bot, update, job_queue, chat_data):
 
     chat_id = update.message.chat_id
